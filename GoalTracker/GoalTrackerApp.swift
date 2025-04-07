28937e1dccdae8acea18e102d3f6367b6236aaad
AJ18 <Ajawaid1807@Gmail.com>
2025-04-07 17:02:47 +0900

    Initial Commit

---
+++ GoalTracker/GoalTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoalTrackerApp.swift
+//  GoalTracker
+//
+//  Created by AJ on 2025/04/07.
+//
+
+import SwiftUI
+
+@main
+struct GoalTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GoalTrackerTests/GoalTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  GoalTrackerTests.swift
+//  GoalTrackerTests
+//
+//  Created by AJ on 2025/04/07.
+//
+
+import Testing
+@testable import GoalTracker
+
+struct GoalTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
